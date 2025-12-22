98ce7528c7fb8796e26b511ab0c35bfbbcf2741b
hariharanrdev <hariharanrdev@gmail.com>
2025-12-22 15:36:15 +0530

    Refactor OOPS.py to correct print statements and update class variable usage

--- OOPS.py
+++ OOPS.py
@@ -3,9 +3,12 @@ class Demo:
     b=20  
 obj1= Demo() #object creation
 obj2= Demo()
-print(obj1.a)
 print(obj1.b)
-print(obj2.a)
+print(obj2.b)
+obj1.a=70
+obj2.b=100
+print(obj1.a)
 print(obj2.b)
 
-
+print(Demo.a)
+print(Demo.b)
