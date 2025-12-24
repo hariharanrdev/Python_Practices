a1daafcf896aa271b560aac2cdcd6a4763fb3f41
hariharanrdev <hariharanrdev@gmail.com>
2025-12-24 14:18:49 +0530

    Implement Bank class with methods for details, deposit, withdraw, and transfer

--- OOPS.py
+++ OOPS.py
@@ -1,14 +1,54 @@
-class Demo:
-    a=10  # Class variable
-    b=20  
-obj1= Demo() #object creation
-obj2= Demo()
-print(obj1.b)
-print(obj2.b)
-obj1.a=70
-obj2.b=100
-print(obj1.a)
-print(obj2.b)
+class Bank:
+    bname = 'power'
+    branch = 'Peelamaedu'
+    ifsc = 'PBI00120'
+    addr = 'nava india'
 
-print(Demo.a)
-print(Demo.b)
+    def __init__(self, name, mob, adr, bal):
+        self.name = name
+        self.mob = mob
+        self.adr = adr
+        self.bal = bal
+
+    def details(self):
+        print(f'=========Bank Details========= \n'
+        f'Bank Name:{self.bname}\n'
+        f'Branch:{self.branch}\n'
+        f'IFSC:{self.ifsc}\n'
+        f'Address:{self.addr}\n'
+        f'=========Customer Details========= \n'
+        f'Name:{self.name}\n'
+        f'Mobile:{self.mob}\n'
+        f'Address:{self.adr}\n'
+        f'Balance:{self.bal}\n'
+        f'{"="*25}')
+
+    def deposit(self, amt):
+        if amt>100:
+            self.bal += amt
+            print(f'{amt} RS successfully Deposited\n'
+            f'Your Total Bal:"{self.bal}"')
+        else:
+            print('Enter Valid amount')
+
+    def withdraw(self, amt):
+        if amt>100:
+            self.bal -= amt
+            print(f'{amt} RS successfully Withdraw\n'
+            f'Your Total Bal:"{self.bal}"')
+        else:
+            print('Enter Valid amount')
+            
+    def transfer(self, amt, Per):
+        if amt>100:
+            self.bal -= amt
+            Per.bal += amt
+            print(f'{amt} RS successfully Transfer to {Per.name}\n'
+            f'Your Total Bal:"{self.bal}"')
+        else:
+            print('Enter Valid amount')
+
+Gowtham = Bank('Gowtham', 9874563210,'cbe',2000)
+Jeni = Bank('Jeni',978965413,'krr',4000)
+Cna = Bank('Cna',9874563210,'cbe',2000)
+madhu = Bank('madhu',9874563210,'cbe',2000)
